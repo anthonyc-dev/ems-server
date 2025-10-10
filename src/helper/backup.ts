5cdb5cabc51ab2689d892497a75d9cddde939062
HardUsername-123 <anthonycrausus14.darkcode@gmail.com>
2025-10-10 08:38:28 +0800

    Refactor StudentRequirement model in schema.prisma for improved clarity; update import statement in backup.ts to use PrismaClient from @prisma/client.

--- src/helper/backup.ts
+++ src/helper/backup.ts
@@ -6,7 +6,7 @@ import {
   LoginRequest,
   RegisterRequest,
 } from "../types/type";
-import { PrismaClient } from "../../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
